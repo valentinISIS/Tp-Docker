27e536f72e90f19577cc3bd4f1cd21fbf2a5c04b
Nicolas Singer <nicolas.singer@gmail.com>
2024-01-03 10:45:30 +0100

    feat(backend): Add CrossOrigin

--- src/main/java/com/isis/backend/Api.java
+++ src/main/java/com/isis/backend/Api.java
@@ -1,14 +1,12 @@
 package com.isis.backend;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api")
 public class Api {
 
